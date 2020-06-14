e448ecdf5c05c758fe7053cf37f52c6d89b9d4ca
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-13 19:25:29 -0700

    Initial

--- graphs/graph.py
+++ graphs/graph.py
@@ -8,7 +8,7 @@ class Vertex(object):
     def __init__(self, vertex_id):
         """
         Initialize a vertex and its neighbors dictionary.
-        
+
         Parameters:
         vertex_id (string): A unique identifier to identify this vertex.
         """
@@ -22,7 +22,7 @@ class Vertex(object):
         Parameters:
         vertex_obj (Vertex): An instance of Vertex to be stored as a neighbor.
         """
-        pass
+        self.__neighbors_dict[vertex_obj.get_id()] = vertex_obj
 
     def __str__(self):
         """Output the list of neighbors of this vertex."""
@@ -59,7 +59,7 @@ class Graph:
     def add_vertex(self, vertex_id):
         """
         Add a new vertex object to the graph with the given key and return the vertex.
-        
+
         Parameters:
         vertex_id (string): The unique identifier for the new vertex.
 
@@ -67,7 +67,7 @@ class Graph:
         Vertex: The new vertex object.
         """
         pass
-        
+
 
     def get_vertex(self, vertex_id):
         """Return the vertex if it exists."""
@@ -86,11 +86,11 @@ class Graph:
         vertex_id2 (string): The unique identifier of the second vertex.
         """
         pass
-        
+
     def get_vertices(self):
         """
         Return all vertices in the graph.
-        
+
         Returns:
         List<Vertex>: The vertex objects contained in the graph.
         """
@@ -157,7 +157,7 @@ class Graph:
         }
 
         # queue of vertices to visit next
-        queue = deque() 
+        queue = deque()
         queue.append(self.get_vertex(start_id))
 
         # while queue is not empty
@@ -187,7 +187,7 @@ class Graph:
     def find_vertices_n_away(self, start_id, target_distance):
         """
         Find and return all vertices n distance away.
-        
+
         Arguments:
         start_id (string): The id of the start vertex.
         target_distance (integer): The distance from the start vertex we are looking for
@@ -196,3 +196,13 @@ class Graph:
         list<string>: All vertex ids that are `target_distance` away from the start vertex
         """
         pass
+
+    def topological_sort(self):
+        indegree0 = []
+        nodes = []
+        next_nodes = {}
+
+
+        while len(indegree0):
+            cur_node = indegree0.pop()
+            nodes.append(cur_node)
