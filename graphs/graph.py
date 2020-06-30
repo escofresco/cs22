a2b76ab82f9cb8e7129271456de81f6795982565
Jonasz Karwacki <jonaszakr@gmail.com>
2020-06-30 03:35:54 -0700

    Complete Tarjan's algorithm and iterative dfs path finding

--- graphs/graph.py
+++ graphs/graph.py
@@ -1,4 +1,5 @@
-from collections import deque
+from collections import defaultdict, deque, namedtuple
+from dataclasses import dataclass
 
 class Vertex(object):
     """
@@ -13,7 +14,7 @@ class Vertex(object):
         vertex_id (string): A unique identifier to identify this vertex.
         """
         self.id = vertex_id
-        self.neighbors_dict = {} # id -> object
+        self.__neighbors_dict = {} # id -> object
 
     def __lt__(self, other_vertex):
         return self.get_id() < other_vertex.get_id()
@@ -59,7 +60,7 @@ class Graph:
         Parameters:
         is_directed (boolean): Whether the graph is directed (edges go in only one direction).
         """
-        self.vertex_dict = {} # id -> object
+        self.__vertex_dict = {} # id -> object
         self.__is_directed = is_directed
 
     @property
@@ -109,10 +110,10 @@ class Graph:
         Returns:
         List<Vertex>: The vertex objects contained in the graph.
         """
-        return list(self.vertex_dict.values())
+        return list(self.__vertex_dict.values())
 
     def contains_id(self, vertex_id):
-        return vertex_id in self.vertex_dict
+        return vertex_id in self.__vertex_dict
 
     def __str__(self):
         """Return a string representation of the graph."""
@@ -151,6 +152,25 @@ class Graph:
                     queue.append(neighbor)
 
         return # everything has been processed
+
+    def find_path_dfs_iter(self, start_id, target_id):
+        """
+        Use DFS with a stack to find a path from start_id to target_id.
+        """
+        stack = [(start_id, [])]
+        seen = set()
+
+        while len(stack):
+            cur_id, path = stack.pop()
+
+            if cur_id not in seen:
+                path.append(cur_id)
+
+                if cur_id == target_id:
+                    return path
+                stack.extend(map(lambda node: (node.get_id(), path),
+                                 self.get_vertex(cur_id).get_neighbors()))
+                seen.add(cur_id)
 
     def find_shortest_path(self, start_id, target_id):
         """
@@ -229,4 +249,102 @@ class Graph:
 
 
     def topological_sort(self):
-        pass
+        raise NotImplemented
+
+    def contains_cycle(self):
+        return self.strongly_connected_components(break_on_cycle=True) is None
+
+    def strongly_connected_components(self, break_on_cycle=False):
+        """
+        Use Tarjan's Algorithm to detect strongly connected components by
+        propogating low-link values thoughout cycles. This fixes the randomness
+        of DFS that restricts low-link values to special cases.
+        Time: O(|V| + |E|)
+
+        Arguments:
+            break_on_cycle: If graph is detected to be cyclic, exit function
+                            early.
+
+        Returns:
+            Tuple of strongly connected components or None if break_on_cycle
+            is True and a cycle is detected.
+        """
+        def dfs(vertex_id):
+            ## When DFS finishes visiting all neighbors of node, check if previous
+            ## node is on stack. If it is, min current node's low-link value
+            ## with previous node's low-link value.
+            nonlocal cur_scc_id, scc_count
+            stack.append(vertex_id)
+            # By creating the vertex_id key in vertexid_to_node_data, it's
+            # incidentally marked as visited
+            vertexid_to_node_data[vertex_id] = NodeData(cur_scc_id, cur_scc_id,
+                                                    True)
+            cur_scc_id += 1
+
+            for neighbor in self.get_vertex(vertex_id).get_neighbors():
+                neighbor_vertex_id = neighbor.get_id()
+
+                if neighbor_vertex_id not in vertexid_to_node_data:
+                    ## neighbor is unvisited
+                    dfs(neighbor_vertex_id)
+
+                if vertexid_to_node_data[neighbor_vertex_id].is_on_stack:
+                    # Change current node's low-link value to min of
+                    # current node's low-link value and neighbor's low-link
+                    # value.
+                    vertexid_to_node_data[vertex_id].lowlink_val = min(
+                        vertexid_to_node_data[vertex_id].lowlink_val,
+                        vertexid_to_node_data[neighbor_vertex_id].lowlink_val)
+
+            cur_node_scc_id = vertexid_to_node_data[vertex_id].scc_id
+            cur_node_lowlink_val = vertexid_to_node_data[vertex_id].lowlink_val
+
+            if (cur_node_scc_id == cur_node_lowlink_val):
+                ## If current node's id equals its low-link value (it has
+                ## started a strongly connected component), then pop nodes off
+                ## stack until current node is reached.
+                scc_count += 1 # New scc found.
+                _vertex_id = stack.pop()
+
+                while _vertex_id != vertex_id:
+                    ## Backtrack; change _vertex_id's low-link value to
+                    ## current vertex_id's scc_id
+                    vertexid_to_node_data[_vertex_id].is_on_stack = False
+                    vertexid_to_node_data[_vertex_id].lowlink_val = cur_node_scc_id
+                    _vertex_id = stack.pop()
+
+                    if break_on_cycle:
+                        # Cycle detected
+                        return
+        @dataclass
+        class NodeData:
+            __slots__ = ("scc_id", "lowlink_val", "is_on_stack")
+            scc_id: int
+            lowlink_val: int
+            is_on_stack: bool
+        node_count = len(self.__vertex_dict)
+        # Map vertex id to strongly connected component (scc) id
+        vertexid_to_node_data = {}
+        stack = [] # Global DFS stack
+        cur_scc_id = 0 # Track current scc id to be assigned
+        scc_count = 0 # Track number of strongly connected components
+
+        for vertex_id, node in self.__vertex_dict.items():
+            if vertex_id not in vertexid_to_node_data:
+                ## Node hasn't been visited
+                ## Use DFS to visit a node, assign it a low-link value and id,
+                ## mark it as visited, and add it to stack
+                dfs(vertex_id)
+
+        if (break_on_cycle and scc_count < node_count or
+            (node_count == 1 and len(self.get_vertices()[0].get_neighbors()) == 1)):
+            # Handle special case where the entire graph is a single connected
+            # component or graph consists of a single node that has a self-loop
+            return
+        # Low-link values which represent distinct strongly connected components
+        lowlink_val_to_vertex_id = defaultdict(list)
+
+        for vertex_id, nodedata in vertexid_to_node_data.items():
+            lowlink_val_to_vertex_id[nodedata.lowlink_val].append(vertex_id)
+        return tuple(map(lambda scc: tuple(scc),
+                         lowlink_val_to_vertex_id.values()))
